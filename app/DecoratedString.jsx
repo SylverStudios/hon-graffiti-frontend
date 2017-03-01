f24ccef2b31918f9cc6fa11a47bc49713f3c673f
Sam Roberts <samgqroberts@gmail.com>
2017-02-28 21:00:01 -0500

    add styling for color of DecoratedString segments

--- app/DecoratedString.jsx
+++ app/DecoratedString.jsx
@@ -2,7 +2,11 @@ import React, { PropTypes } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 
 const DecoratedString = ({ segments }) => (
-  <span>{segments.map(segment => segment.content)}</span>
+  <span>
+    {segments.map(segment => (
+      <span style={{ color: segment.color }}>{segment.content}</span>
+    ))}
+  </span>
 );
 DecoratedString.propTypes = {
   segments: ImmutablePropTypes.listOf(PropTypes.object).isRequired,
