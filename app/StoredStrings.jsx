3e959d96af65ef52579b7520d18bab1e496f165c
Sam Roberts <samgqroberts@gmail.com>
2017-03-12 01:22:29 -0500

    space out stored strings a bit better

--- app/StoredStrings.jsx
+++ app/StoredStrings.jsx
@@ -8,7 +8,12 @@ import style from './StoredStrings.scss';
 export default connect(({ strings }) => ({ strings }))(({ strings }) => (
   <div className={style.component}>
     {strings.map((decorated, raw) => (
-      <div key={raw}>{raw}: <DecoratedString segments={decorated} /></div>
+      <div key={raw} className={style.stringContainer}>
+        <div className={style.raw}>{raw}</div>
+        <div className={style.rendered}>
+          <DecoratedString segments={decorated} />
+        </div>
+      </div>
     )).toArray()}
   </div>
 ));
--- test-resources/StartingStrings.js
+++ test-resources/StartingStrings.js
@@ -7,7 +7,12 @@ export default function generate() {
     '^rRed 123 ^wWhite and ^bBlue^w, baby!',
     'another 123 ^gstring',
     'This is a 123string with no formatting',
-    '^rRed ^wWhite and123  ^bBlue^w, baby!',
+    '^rRed ^wWhite and123  ^bBlue^w, baby!' +
+    '^rRed ^wWhite and ^bBlue^w, baby! ^rRed' +
+    ' ^wWhite and ^bBlue^w, baby! ^rRed' +
+    ' ^wWhite and ^bBlue^w, baby! ^rRed ^wWhite' +
+    ' and ^bBlue^w, baby! ^rRed' +
+    ' ^wWhite and ^bBlue^w, baby!',
     'another ^gstring123',
   ];
 }
