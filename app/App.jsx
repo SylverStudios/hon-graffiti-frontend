4a7e37d00f2f36383a84a40a9f24604b8b208373
Sam Roberts <samgqroberts@gmail.com>
2017-03-11 23:36:51 -0500

    add bar at top of screen
    to hold logo and login form / logged in user info

--- app/App.jsx
+++ app/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import TopBar from './TopBar';
 import StoredStrings from './StoredStrings';
 import StringInput from './StringInput';
 
 export default () => (
   <div>
+    <TopBar />
     Hon graffiti frontend <br />
     <StoredStrings />
     <StringInput />
