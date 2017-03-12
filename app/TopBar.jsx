f2640e436fb0281d3e830f1768c8657f83355f91
Sam Roberts <samgqroberts@gmail.com>
2017-03-11 23:46:08 -0500

    add logo and login information placeholder to TopBar

--- app/TopBar.jsx
+++ app/TopBar.jsx
@@ -3,5 +3,10 @@ import React from 'react';
 import style from './TopBar.scss';
 
 export default () => (
-  <div className={style.component} />
+  <div className={style.component}>
+    <div className={style.inner}>
+      <div className={style.logo}>Drastic</div>
+      <div className={style.userInfo}>User Accounts not available yet.</div>
+    </div>
+  </div>
 );
