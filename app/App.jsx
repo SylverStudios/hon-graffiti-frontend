43a5ffca31ad82c7ae5758ca24d34859f6482ff8
Sam Roberts <samgqroberts@gmail.com>
2017-03-11 21:10:33 -0500

    add default starting strings
    for more easy development on a populated list.
    until we can hook into an API

--- app/App.jsx
+++ app/App.jsx
@@ -5,10 +5,17 @@ import StoredStrings from './StoredStrings';
 import StringInput from './StringInput';
 import parseRaw from './parseRaw';
 
+import StartingStrings from '../test-resources/StartingStrings';
+
+const startingStrings =
+  Map(List(StartingStrings()).toObject()).mapEntries(entry => (
+    [entry[1], new List(parseRaw(entry[1]))]
+  ));
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { strings: new Map() };
+    this.state = { strings: startingStrings };
     this.addString = this.addString.bind(this);
   }
 
---
+++ test-resources/StartingStrings.js
@@ -0,0 +1,13 @@
+export default function generate() {
+  return [
+    'This is a string with no formatting',
+    '^rRed ^wWhite and ^bBlue^w, baby!',
+    'another ^gstring',
+    'This 123 is a string with no formatting',
+    '^rRed 123 ^wWhite and ^bBlue^w, baby!',
+    'another 123 ^gstring',
+    'This is a 123string with no formatting',
+    '^rRed ^wWhite and123  ^bBlue^w, baby!',
+    'another ^gstring123',
+  ];
+}
